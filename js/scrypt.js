698744deda778c936a272af0d3e83ecbe9011c21
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-09-23 18:34:48 +0200

    Añadidos comentarios en el archivo JavaScript para la explicación del código

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,41 +1,56 @@
-let power = false
+// Almacena en variables los elementos del DOM necesarios
 let lightPower = document.querySelector("#lightPower")
-let audio = new Audio()
-audio.src = "../audio/street-fighter-intro.mp3"
 let start = document.querySelector("#start")
 let screen = document.querySelector("#screen")
-let animationInProgress = false
 let image1 = '<img src="../images/gta.webp">'
 let image2 = '<img src="../images/scorpion.jpeg"></img>'
 let image3 = '<img src="../images/kratos.webp"></img>'
-let images = [image1, image2, image3]
-let imgaesPosition = 0
 let buttonA = document.querySelector("#buttonA")
 let buttonB = document.querySelector("#buttonB")
+// Rastrea si la consola está encendida
+let power = false
+// Rastrea si el video inicial está reproduciendose
+let animationInProgress = false
+// Almacena en un array las imágenes y crea una variable para su posición en la pantalla
+let images = [image1, image2, image3]
+let imgaesPosition = 0
+// Crea un audio para utilizarlo al iniciar la consola
+let audio = new Audio()
+audio.src = "../audio/street-fighter-intro.mp3"
 
+// Al hacer click en el botón START...
 start.addEventListener("click", () => {
+    // Si la consola no está encendida...
     if (!power) {
+        // La enciende y aplica las clases correspondientes a la luz
         power = true
         lightPower.classList.toggle("powerOn")
         lightPower.classList.toggle("powerOff")
+        // Ejecuta la animación de encendido
         screen.innerHTML = '<img src="https://i.gifer.com/XTrC.gif"></img>'
         audio.play();
         animationInProgress = true
+        // Luego de finalizada la animación muestra la primera imagen
         setTimeout(() => {
             screen.innerHTML = images[0];
             animationInProgress = false
         }, 4000)
+    // Si la consola está encendida...
     } else if (power && !animationInProgress) {
+        // La apaga y aplica las clases correspondientes a la luz
         power = false
         lightPower.classList.toggle("powerOn")
         lightPower.classList.toggle("powerOff")
+        // Deja la pantalla vacía y reinicia el indicador de la posición de las imágenes
         screen.innerHTML = ""
         imgaesPosition = 0
+        // Pausa el audio y lo setea en el momento 0 de su duración
         audio.pause();
         audio.currentTime = 0
     }
 })
 
+// Se encarga de avanzar en lás imágenes siempre y cuando la consola esté encendida y no se esté reproducioendo la animación de encendido
 buttonA.addEventListener("click", () => {
     if (power && !animationInProgress) {
         if (imgaesPosition === 2) {
@@ -48,6 +63,7 @@ buttonA.addEventListener("click", () => {
     }
 })
 
+// Se encarga de retroceder en lás imágenes siempre y cuando la consola esté encendida y no se esté reproducioendo la animación de encendido
 buttonB.addEventListener("click", () => {
     if (power && !animationInProgress) {
         if (imgaesPosition === 0) {
